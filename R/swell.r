95ea3e9838cbf7167487aea49b65ce6ba49d3c0d
JooYoung Seo <sjysky@gmail.com>
2017-02-20 22:44:52 -0500

    R Packaging

--- R/swell.r
+++ R/swell.r
@@ -1,14 +1,35 @@
-#Swell function developed by JooYoung Seo.
+#' swell
+#' @title Generate and optimize a braille-embedded ready-to-print pdf file for swell form machine
 
-#Saving font names into variables for convenience
-BN = "Braille Normal"
-SB = "Swell Braille"
-AR = "Arial"
+#' @aliases swell
+#' @keywords swell
 
-#Line Types here
-single = c(1)
-two = c(1, 3)
-three = c(1, 3, 5)
+#' @description  The first argument to this function must be a call to create a graph, such as a histogram. Instead of opening a new graphics device, the graph will be created in a pdf file, with all text being presented using  a braille font.
+
+#' @export swell
+#' @param x the call to create a graph
+#' @param file A character string giving the filename where the image is to be saved. The default file name is "output.pdf" if not given.
+#' @param lines a vector argument that is passed to lty. Specify this as c(1, 3) when two lines are necessary (e.g., ANOVA interaction plot); and c(1, 3, 5) for three lines required plots.
+
+#' @details
+#' *Note*: This function optimizes for both 11*11 and 11*8.5 swell touch papers. When printing a generated braille pdf, just remember two things:
+#' 1. check "Fit" in the print dialog box (the shortcut key is ALT+F).
+#' 2. Check "Landscape" in the print dialog box as well.
+#' * For boxplot, you do not need to check the "Landscape," just leave it as a "Portray." However, please check the "Fit" all the time.
+
+#' *Information*:
+#' If you are not familiar with what swell form machine is, please refer to [this web site](http://www.americanthermoform.com/product/swell-form-graphics-ii-machine/).
+
+#' @return Nothing within the R session, but a pdf file will be created in the user's working directory.
+
+
+#' @examples
+#' swell(hist(airquality$Ozone), "test.pdf")
+#' # The braille ready-to-print pdf version will be created in your working directory. Print it out and run the swell touch paper through swell form heating machine.
+
+#' @author JooYoung Seo
+
+
 
 #Function starts
 swell <-
@@ -73,9 +94,9 @@ if (!dir.exists('fonts')) {
 #}
 
 #Preparing PDF with braille font
+if (!"Braille Normal" %in% extrafont::fonts()) {
 font_import(pattern = "BRAILLE1.ttf", paths=system.file("fonts", package="BrailleR"), prompt=FALSE)
-#font_import(pattern = "Swell-Braille.ttf", paths="fonts", prompt=FALSE)
-
+}
 
 #Showing graphic
 #windowsFonts(A=windowsFont("Braille Normal"))
@@ -99,7 +120,6 @@ family =
 #Graphic code below:
 
 sink("description.txt")
-#source("graphic.r")
 BrailleR::VI(x)
 sink()
 sink("warnings.txt")
